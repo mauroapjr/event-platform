4fbbbd4683a94c792e07ce0635c38e017faa43f7
mauroapjr <mauroapjr@outlook.com>
2024-05-16 17:05:41 -0400

    clean up

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import HomePage from './HomePage';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import HomePage from "./HomePage";
+import reportWebVitals from "./reportWebVitals";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles/App.css';
-import './styles/index.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <HomePage />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 reportWebVitals();
