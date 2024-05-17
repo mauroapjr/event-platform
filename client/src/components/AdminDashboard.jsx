22c22d8a1bbabe32db5c08217b1e4f3a304ec49e
mauroapjr <mauroapjr@outlook.com>
2024-05-17 13:26:58 -0400

    created AdminDashboard page

---
+++ client/src/components/AdminDashboard.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const AdminDashboard = () => {
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-12">
+          <h2>Admin Dashboard</h2>
+          <p>admin dashboard!</p>          
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AdminDashboard;
