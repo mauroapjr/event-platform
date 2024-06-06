0bd50e05834d487f62af28a0fef98d73681c53ba
mauroapjr <mauroapjr@outlook.com>
2024-06-06 10:49:52 -0400

    changed database access to AWS RDS

--- server/db.mjs
+++ server/db.mjs
@@ -1,12 +1,26 @@
+// import pkg from 'pg';
+// const { Pool } = pkg;
+
+// const pool = new Pool({
+//   user: 'postgres',
+//   host: 'localhost',
+//   database: 'event_platform',
+//   password: 'password',
+//   port: 5432,
+// });
+
+// export default pool;
+
 import pkg from 'pg';
 const { Pool } = pkg;
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'event_platform',
-  password: 'password',
-  port: 5432,
+  user: 'diogo_developer',  
+  host: 'dbeventplatform.cjqu32oisej5.us-east-1.rds.amazonaws.com',  
+  database: 'dbEventPlatform', 
+  password: 'password123',  
+  port: 5432,  
 });
 
 export default pool;
+
