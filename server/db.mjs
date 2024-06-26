587b9090c2c5b882ee2867b41178e1a86e8c11cb
mauroapjr <mauroapjr@outlook.com>
2024-06-26 15:54:22 -0400

    database recovered

--- server/db.mjs
+++ server/db.mjs
@@ -11,21 +11,43 @@
 
 // export default pool;
 
-import pkg from 'pg';
+// import pkg from 'pg';
+// const { Pool } = pkg;
+
+// const pool = new Pool({
+//   user: 'diogo_developer',
+//   host: 'dbeventplatform.cjqu32oisej5.us-east-1.rds.amazonaws.com',
+//   database: 'dbEventPlatform',
+//   password: 'password123',
+//   port: 5432,
+//   ssl: {
+//     rejectUnauthorized: false
+//   },
+// });
+
+// export default pool;
+
+import pkg from "pg";
 const { Pool } = pkg;
 
 const pool = new Pool({
-  user: 'diogo_developer',  
-  host: 'dbeventplatform.cjqu32oisej5.us-east-1.rds.amazonaws.com',  
-  database: 'dbEventPlatform', 
-  password: 'password123',  
-  port: 5432,  
+  user: "diogo_developer",
+  host: "dbeventplatform.cjqu32oisej5.us-east-1.rds.amazonaws.com",
+  database: "dbEventPlatform",
+  password: "password123",
+  port: 5432,
   ssl: {
-    rejectUnauthorized: false 
+    rejectUnauthorized: false,
   },
 });
 
+(async () => {
+  try {
+    const res = await pool.query("SELECT current_user;");
+    console.log("Current DB user:", res.rows[0]);
+  } catch (err) {
+    console.error("Error fetching current user:", err);
+  }
+})();
+
 export default pool;
-
-
-
